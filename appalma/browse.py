b80b362863f235854b5ca15cb0fd6d19bec3f763
Rachel Alcraft <rachel.alcraft@icr.ac.uk>
2025-05-12 19:17:15 +0100

    play_inner indent

--- appalma/browse.py
+++ appalma/browse.py
@@ -48,79 +48,79 @@ class BrowseView():
                 else:
                     self.play_inner(cols)
 
-        def play_inner(self):        
-            self.cmd_dir = CmdSSH(self.ssh, cmd=self.txt_dir, output="list", spinner="Retrieving files")
-            self.cmd_dir.play()            
-            # remove hpcuser from the list
-            
-            if len(self.cmd_dir.result) > 0:
-                # retrieve user lists on this basis
-                # remove the path from the result                        
-                # replace all the paths in the path with ""
-                self.files_list = [x.replace(self.folder, "") for x in self.cmd_dir.result]                    
-                self.sel_file = self.files_list[0]                                        
-                self.txt_contents = self.ssh.read_file(self.folder + self.sel_file)
-                # get file stats
-                if self.include_stat:
-                    self.cmd_file = CmdSSH(self.ssh, cmd=f"stat {self.folder + self.sel_file}", output="none", spinner="stat")
-                    self.cmd_file.play()
-                                                                                                                                                                                                    
-            if self.cmd_dir:
-                if not self.cmd_dir.ok:
-                    st.error(f"FAILED: {self.cmd_dir.error}")
-                else:                                                
-                    def change_sel_file():
-                        self.sel_file = st.session_state.file
-                        # retrieve user lists on this basis                    
-                        self.txt_contents = self.ssh.read_file(self.folder + self.sel_file)
-                        # get file stats
-                        if self.include_stat:
-                            self.cmd_file = CmdSSH(self.ssh, cmd=f"stat {self.folder + self.sel_file}", output="none", spinner="stat")
-                            self.cmd_file.play()
-                                                                                                        
-                    # find index of selected group                
-                    with cols[0]:
-                        self.sel_file = st.radio("Select a file:", self.files_list, index=0, key=f"{self.folder_key}_radio",on_change=change_sel_file)
+    def play_inner(self):        
+        self.cmd_dir = CmdSSH(self.ssh, cmd=self.txt_dir, output="list", spinner="Retrieving files")
+        self.cmd_dir.play()            
+        # remove hpcuser from the list
+        
+        if len(self.cmd_dir.result) > 0:
+            # retrieve user lists on this basis
+            # remove the path from the result                        
+            # replace all the paths in the path with ""
+            self.files_list = [x.replace(self.folder, "") for x in self.cmd_dir.result]                    
+            self.sel_file = self.files_list[0]                                        
+            self.txt_contents = self.ssh.read_file(self.folder + self.sel_file)
+            # get file stats
+            if self.include_stat:
+                self.cmd_file = CmdSSH(self.ssh, cmd=f"stat {self.folder + self.sel_file}", output="none", spinner="stat")
+                self.cmd_file.play()
+                                                                                                                                                                                                
+        if self.cmd_dir:
+            if not self.cmd_dir.ok:
+                st.error(f"FAILED: {self.cmd_dir.error}")
+            else:                                                
+                def change_sel_file():
+                    self.sel_file = st.session_state.file
+                    # retrieve user lists on this basis                    
+                    self.txt_contents = self.ssh.read_file(self.folder + self.sel_file)
+                    # get file stats
+                    if self.include_stat:
+                        self.cmd_file = CmdSSH(self.ssh, cmd=f"stat {self.folder + self.sel_file}", output="none", spinner="stat")
+                        self.cmd_file.play()
+                                                                                                    
+                # find index of selected group                
+                with cols[0]:
+                    self.sel_file = st.radio("Select a file:", self.files_list, index=0, key=f"{self.folder_key}_radio",on_change=change_sel_file)
 
-                    if len(self.displays) > 0:
-                        with cols[1]:
-                            #st.write(f"Selected file = {self.sel_file}")
-                            tabs = st.tabs(self.displays)
-                            for t in range(len(tabs)):
-                                with tabs[t]:                                                        
-                                    if self.displays[t] == "text":                                
-                                        st.text(self.txt_contents)
-                                    elif self.displays[t] == "code":
-                                        st.code(self.txt_contents)
-                                    elif self.displays[t] == "info":     
-                                        if self.include_stat:
-                                            st.code(self.cmd_file.result)
-                                        else:
-                                            st.warning("No file stats available, please include them in the command")
-                                    elif self.displays[t] in ["df","plot"]:
-                                        try:
-                                            df = pd.read_csv(StringIO(self.txt_contents))                                    
-                                            if self.displays[t] == "df":
-                                                st.dataframe(df)                                
-                                            elif self.displays[t] == "plot":
-                                                keys = df.columns
-                                                cols = st.columns(3)
-                                                with cols[0]:
-                                                    x = st.selectbox("X axis", keys)
-                                                with cols[1]:
-                                                    y = st.selectbox("Y axis", keys)
-                                                with cols[2]:
-                                                    z = st.selectbox("Z axis", keys)
-                                                
-                                                
-                                                st.warning("Plot not implemented yet!")
-                                        except Exception as e:
-                                            st.error(f"Error parsing output: {e}")                                
+                if len(self.displays) > 0:
+                    with cols[1]:
+                        #st.write(f"Selected file = {self.sel_file}")
+                        tabs = st.tabs(self.displays)
+                        for t in range(len(tabs)):
+                            with tabs[t]:                                                        
+                                if self.displays[t] == "text":                                
+                                    st.text(self.txt_contents)
+                                elif self.displays[t] == "code":
+                                    st.code(self.txt_contents)
+                                elif self.displays[t] == "info":     
+                                    if self.include_stat:
+                                        st.code(self.cmd_file.result)
                                     else:
-                                        st.error(f"Unknown display type: {self.displays[t]}")
-                            
+                                        st.warning("No file stats available, please include them in the command")
+                                elif self.displays[t] in ["df","plot"]:
+                                    try:
+                                        df = pd.read_csv(StringIO(self.txt_contents))                                    
+                                        if self.displays[t] == "df":
+                                            st.dataframe(df)                                
+                                        elif self.displays[t] == "plot":
+                                            keys = df.columns
+                                            cols = st.columns(3)
+                                            with cols[0]:
+                                                x = st.selectbox("X axis", keys)
+                                            with cols[1]:
+                                                y = st.selectbox("Y axis", keys)
+                                            with cols[2]:
+                                                z = st.selectbox("Z axis", keys)
+                                            
+                                            
+                                            st.warning("Plot not implemented yet!")
+                                    except Exception as e:
+                                        st.error(f"Error parsing output: {e}")                                
+                                else:
+                                    st.error(f"Unknown display type: {self.displays[t]}")
                         
-                            
+                    
+                        
 
 
                     
