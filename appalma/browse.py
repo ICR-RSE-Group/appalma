82f3a7a9ac7b442573d266fadb3569feb5a6f66b
Rachel Alcraft <rachel.alcraft@icr.ac.uk>
2025-05-12 14:08:52 +0100

    add a key for list

--- appalma/browse.py
+++ appalma/browse.py
@@ -32,7 +32,7 @@ class BrowseView():
             self.txt_dir = st.text_input("Search",self.txt_dir)
         cols = st.columns([2,5])                    
         with cols[0]:
-            if st.button("List files"):            
+            if st.button("List files", key=self.folder_key + "_lst_btn"):
                 self.cmd_dir = CmdSSH(self.ssh, cmd=self.txt_dir, output="list", spinner="Retrieving files")
                 self.cmd_dir.play()            
                 # remove hpcuser from the list
